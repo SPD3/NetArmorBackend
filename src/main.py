36451a2bb976d82b2658761713a40d69c87f983d
SPD3 <sean@ayerdoyles.org>
2023-10-11 19:52:01 -0400

    Working graphql integration with postgresql

--- src/main.py
+++ src/main.py
@@ -3,21 +3,11 @@ import strawberry
 
 from fastapi import Depends, FastAPI, HTTPException
 from strawberry.fastapi import GraphQLRouter
+from strawberry.types import Info
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
-
-@strawberry.type
-class Query:
-    @strawberry.field
-    def hello(self) -> str:
-        return "Hello World"
-
-
-schema = strawberry.Schema(Query)
-
-graphql_app = GraphQLRouter(schema)
 
 
 app = FastAPI()
@@ -25,9 +15,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-app.include_router(graphql_app, prefix="/graphql")
-
 
 models.Base.metadata.create_all(bind=engine)
 # Dependency
@@ -58,3 +45,31 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+async def get_context(
+    db=Depends(get_db),
+):
+    return {
+        "db": db,
+    }
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
+    
+    @strawberry.field
+    def user(self, id : int, info: Info) -> str:
+        db = info.context['db']
+        db_user = crud.get_user(db, user_id=id)
+        if db_user is None:
+            return "No user with id: " + str(id)
+        return "user email is: " + str(db_user.email)
+
+schema = strawberry.Schema(Query)
+
+graphql_app = GraphQLRouter(schema, context_getter=get_context)
+
+app.include_router(graphql_app, prefix="/graphql")
