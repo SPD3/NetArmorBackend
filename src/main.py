217252171b93be72975ccf3c092a95a9259598fa
SPD3 <sean@ayerdoyles.org>
2023-10-11 19:54:52 -0400

    Ensured that database models were created f irst

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -16,7 +17,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-models.Base.metadata.create_all(bind=engine)
 # Dependency
 def get_db():
     db = SessionLocal()
