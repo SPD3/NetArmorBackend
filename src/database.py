9e32ddcb94abdf0617d8b9f5242c7e90550a70b5
SPD3 <sean@ayerdoyles.org>
2023-10-25 08:54:15 -0400

    Trying to turn database into singleton

--- src/database.py
+++ src/database.py
@@ -1,14 +1,30 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from src.config import settings
-
-
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://" + settings.POSTGRES_USER + ":" + settings.POSTGRES_PASSWORD+ "@"+ settings.POSTGRES_NAME + ":"+ settings.POSTGRES_PORT+ "/" + settings.POSTGRES_DB
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from . import models
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 Base = declarative_base()
+
+class Database():
+    def __init__(self) -> None:
+        self.url = "postgresql+psycopg2://" + settings.POSTGRES_USER + ":" + settings.POSTGRES_PASSWORD+ "@"+ settings.POSTGRES_NAME + ":"+ settings.POSTGRES_PORT+ "/" + settings.POSTGRES_DB
+        self.engine = create_engine(
+            self.url
+        )
+        models.Base.metadata.create_all(bind=self.engine)
+        self.session = scoped_session(sessionmaker(autoflush=True, autocommit=False))
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Database, cls).__new__(cls)
+        return cls.instance
+    
+    def get_url(self):
+        return self.url
+    
+    def get_engine(self):
+        return self.engine
+    
+    def get_session(self):
+        return self.session
--- src/main.py
+++ src/main.py
@@ -1,59 +1,65 @@
-from . import crud, models 
+from . import crud 
+from .database import Database
 from src.config import settings
-from .database import engine
-
-from src.database import SQLALCHEMY_DATABASE_URL
 from tg.util import Bunch
-from sqlalchemy.orm import scoped_session, sessionmaker
 
 from src.netarmor_api import App, Endpoint
 
 
-models.Base.metadata.create_all(bind=engine)
-DBSession = scoped_session(sessionmaker(autoflush=True, autocommit=False))
 
-def init_model(engine):
-    DBSession.configure(bind=engine)
 
 def get_users(skip: int = 0, limit: int = 100):
-    users = crud.get_users(DBSession, skip=skip, limit=limit)
+    db_session = Database().get_session()
+    users = crud.get_users(db_session, skip=skip, limit=limit)
     ret = []
     for i, user in enumerate(users):
         ret.append({"id" : i, "email" : user.email})
     return ret
 
 def check_credential(email, password):
-    db_user = crud.get_user_by_email(DBSession, email=email)
+    db_session = Database().get_session()
+    db_user = crud.get_user_by_email(db_session, email=email)
     if db_user is None:
         return {"data" : False}
     return db_user.password == password
 
 
 def account_exists(email, password):
-    db_user = crud.get_user_by_email(DBSession, email=email)
+    db_session = Database().get_session()
+    db_user = crud.get_user_by_email(db_session, email=email)
     return db_user is not None
 
 def create_account(email, password):
-    db_user = crud.get_user_by_email(DBSession, email=email)
+    db_session = Database().get_session()
+    db_user = crud.get_user_by_email(db_session, email=email)
     #TODO: Look into better error handling so that the react side can see this...
     if db_user:
         return False
-    crud.create_user(db=DBSession, email=email, password=password)
+    crud.create_user(db=db_session, email=email, password=password)
     return True
 
 def basic_endpoint():
     return "Hello World"
 
-app = App()
+def register_endpoints(app:App):
+    app.register_endpoint("users", get_users, Endpoint.GET)
+    app.register_endpoint("CheckCredentials", check_credential, Endpoint.GET)
+    app.register_endpoint("AccountExists", account_exists, Endpoint.GET)
+    app.register_endpoint("CreateAccount", create_account, Endpoint.POST)
+    app.register_endpoint("basic_endpoint", basic_endpoint, Endpoint.GET)
 
-app.register_endpoint("users", get_users, Endpoint.GET)
-app.register_endpoint("CheckCredentials", check_credential, Endpoint.GET)
-app.register_endpoint("AccountExists", account_exists, Endpoint.GET)
-app.register_endpoint("CreateAccount", create_account, Endpoint.POST)
-app.register_endpoint("basic_endpoint", basic_endpoint, Endpoint.GET)
 
-model_bunch = Bunch(
-    DBSession=DBSession,
-    init_model=init_model
-)
-app.run(settings.DATABASE_API_HOST_NAME, int(settings.DATABASE_API_PORT), SQLALCHEMY_DATABASE_URL, model_bunch)
+def main():
+    db = Database()
+    app = App()
+    register_endpoints(app)
+    
+    
+    def init_model(engine):
+        db.get_session().configure(bind=engine)
+
+    model_bunch = Bunch(
+        DBSession=db.get_session(),
+        init_model=init_model
+    )
+    app.run(settings.DATABASE_API_HOST_NAME, int(settings.DATABASE_API_PORT), db.get_url(), model_bunch)
