a4a5660b85fc12a82a24073e8f534adb54afc00e
Jimmy Shong <jimmysh341@gmail.com>
2023-10-28 12:24:02 -0400

    Added New Endpoint and Tests

--- src/models.py
+++ src/models.py
@@ -10,3 +10,16 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
+
+class WebsiteOwner(Base):
+    __tablename__ = 'website_owners'
+    
+    email = Column(String, primary_key=True, nullable=False, index=True)
+    password = Column(String, nullable=False)
+
+class Website(Base):
+    __tablename__ = 'websites'
+    
+    url = Column(String, primary_key=True, nullable=False)
+    owner = Column(String, ForeignKey('website_owners.email'), nullable=False)
+    website_name = Column(String, nullable=False)
