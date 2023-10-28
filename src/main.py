193ca8df9cb72ac710a5a4637c9b74c0f966078f
Jimmy Shong <jimmysh341@gmail.com>
2023-10-28 12:28:39 -0400

    Revert most of "Added New Endpoint and Tests"
    This reverts commit a4a5660b85fc12a82a24073e8f534adb54afc00e.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from . import crud
 from .database import Database
 from src.config import settings
 from tg.util import Bunch
-from src import models
 
 from src.netarmor_api import App, Endpoint
 
@@ -21,6 +20,7 @@ def check_credential(email, password):
         return False
     return db_user.password == password
 
+
 def account_exists(email, password):
     db_session = Database().get_session()
     db_user = crud.get_user_by_email(db_session, email=email)
@@ -37,71 +37,6 @@ def create_account(email, password):
 
 def basic_endpoint():
     return "Hello World"
-
-def create_website_owner(email, password):
-    db_session = Database().get_session()
-    db_user = db_session.query(models.WebsiteOwner).filter(models.WebsiteOwner.email == email).first()
-    if db_user:
-        return False
-    website_owner = models.WebsiteOwner(email=email, password=password)
-    db_session.add(website_owner)
-    db_session.commit()
-    db_session.refresh(website_owner)
-    return True
-
-def website_owner_exists(email):
-    db_session = Database().get_session()
-    db_website_owner = db_session.query(models.WebsiteOwner).filter(models.WebsiteOwner.email == email).first()
-    return db_website_owner is not None
-
-def get_website_owners(skip: int = 0, limit: int = 100):
-    db_session = Database().get_session()
-    website_owners = db_session.query(models.WebsiteOwner).offset(skip).limit(limit).all()
-    ret = []
-    for i, website_owner in enumerate(website_owners):
-        ret.append({"email" : website_owner.email, "password" :  website_owner.password})
-    return ret
-
-def get_website_owner(email):
-    db_session = Database().get_session()
-    db_website_owner = db_session.query(models.WebsiteOwner).filter(models.WebsiteOwner.email == email).first()
-    ret = {"email" : db_website_owner.email, "password" :  db_website_owner.password}
-    return ret
-
-def update_website_owner_password(email, password):
-    db_session = Database().get_session()
-    db_website_owner = db_session.query(models.WebsiteOwner).filter(models.WebsiteOwner.email == email).first()
-    if db_website_owner is None:
-        return False
-    db_website_owner.password = password
-    db_session.commit()
-    db_session.refresh(db_website_owner)
-    return True
-
-def delete_website_owner(email):
-    db_session = Database().get_session()
-    db_website_owner = db_session.query(models.WebsiteOwner).filter(models.WebsiteOwner.email == email).first()
-    if db_website_owner is None:
-        return False
-    db_session.delete(db_website_owner)
-    db_session.commit()
-    return True
-
-def add_website(url, email, website_name):
-    db_session = Database().get_session()
-    db_url = db_session.query(models.Website).filter(models.Website.url == url).first()
-    if db_url:
-        return False
-    website = models.Website(url=url, owner=email, website_name=website_name)
-    db_session.add(website)
-    db_session.commit()
-    db_session.refresh(website)
-    return True
-
-def website_exists(url):
-    db_session = Database().get_session()
-    db_website = db_session.query(models.WebsiteOwner).filter(models.Website.url == url).first()
-    return db_website
 
 def register_endpoints(app:App):
     app.register_endpoint("users", get_users, Endpoint.GET)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 import pytest
-from src.main import *
+from src.main import basic_endpoint, create_account, account_exists
 
 def test_basic():
     assert 1 == 1
@@ -11,37 +11,4 @@ def test_basic_endpoint():
 def test_create_user():
     create_account("micky@mouse.com", "12345")
     assert account_exists("micky@mouse.com", "12345")
-
-def test_create_website_owner():
-    create_website_owner("micky@mouse.com", "12345")
-    assert website_owner_exists("micky@mouse.com")
-
-def test_get_website_owners():
-    create_website_owner("micky@mouse.com", "12345")
-    create_website_owner("minnie@mouse.com", "6789")
-    assert (get_website_owners() == [{"email" : "micky@mouse.com", "password" :  "12345"},
-                                    {"email" : "minnie@mouse.com", "password" :  "6789"}])
-
-def test_get_website_owner():
-    create_website_owner("micky@mouse.com", "12345")
-    create_website_owner("minnie@mouse.com", "6789")
-    assert (get_website_owner("micky@mouse.com") == {"email" : "micky@mouse.com", "password" :  "12345"})
-    
-def test_update_website_owner_password():
-    create_website_owner("micky@mouse.com", "12345")
-    update_website_owner_password("micky@mouse.com", "6789")
-    assert (get_website_owner("micky@mouse.com")["password"] == "6789")
-
-def test_delete_website_owner():
-    create_website_owner("micky@mouse.com", "12345")
-    delete_website_owner("micky@mouse.com")
-    assert not website_owner_exists("micky@mouse.com")
-
-def test_add_website():
-    create_website_owner("micky@mouse.com", "12345")
-    add_website("https://jiminator.github.io/Midterm/", "micky@mouse.com", "jimmy's website")
-    
-    db_session = Database().get_session()
-    db_session.add()
-    assert website_exists("https://jiminator.github.io/Midterm/")
     
