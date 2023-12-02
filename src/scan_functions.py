db45bdd565edfd1cd3b5ee55c3da068f64a8a11d
SPD3 <sean@ayerdoyles.org>
2023-12-02 17:50:32 -0500

    two scan results functions

--- src/scan_functions.py
+++ src/scan_functions.py
@@ -1,8 +1,8 @@
 from src import models
 
 # Scan Functions
-def check_scan(db_session, scan_id, website_owner, website_url, deep, date):
-    scans = db_session.query(models.Scan).filter(models.Scan.scan_id==scan_id).all()
+def check_scan(db_session,website_owner, website_url, deep, date):
+    scans = db_session.query(models.Scan).filter(models.Scan.website_owner==website_owner).filter(models.Scan.website_url==website_url).filter(models.Scan.deep==deep).filter(models.Scan.date==date).all()
     if len(scans) != 1:
         return False
     scan = scans[0]
@@ -11,6 +11,7 @@ def check_scan(db_session, scan_id, website_owner, website_url, deep, date):
             and scan.deep == deep
             and scan.date == date)
 
-def add_scan(db_session, scan_id, owner_email, url, deep, date):
-    db_scan = models.Scan(scan_id=scan_id, website_owner=owner_email, website_url=url, deep=deep, date=date)
+def add_scan(db_session, owner_email, url, deep, date):
+    db_scan = models.Scan(website_owner=owner_email, website_url=url, deep=deep, date=date)
     db_session.add(db_scan)
+    return db_scan.scan_id
---
+++ src/website_owner_endpoints.py
@@ -0,0 +1,43 @@
+from src import models
+from src.scan_functions import add_scan
+from src.vulnerability_functions import add_tested_vulnerability
+from typing import Dict
+import datetime
+from .database import Database
+
+
+def get_scan_results(email:str):
+    db_session = Database().get_session()
+    scans = db_session.query(models.Scan).filter(models.Scan.website_owner==email).all()
+    return_res = []
+    for scan in scans:
+        return_res.append({})
+        res = {}
+        vulnerabilities = scan.tested_vulnerabilities
+        for vulnerability in vulnerabilities:
+            res[vulnerability.vulnerability] = {
+                "score" : vulnerability.score,
+                "success" : vulnerability.success,
+                "description" : vulnerability.description
+            }
+        return_res[-1]["res"] = res
+        return_res[-1]["url"] = scan.website_url
+        return_res[-1]["date"] = scan.date
+
+
+    return return_res
+
+def add_scan_result(scan_results:Dict[str,object]):
+    account_email = scan_results["email"]
+    result = scan_results["res"]
+    url = scan_results["url"]
+    is_deep_scan = scan_results["scan_type"] == "deep_scan"
+    scan_time = datetime.datetime.now()
+    db_session = Database().get_session()
+    scan_id = add_scan(db_session=db_session, owner_email=account_email, url=url, deep=is_deep_scan, date=scan_time)
+    for vulnerability in result:
+        add_tested_vulnerability(db_session, scan_id, vulnerability, result[vulnerability]["score"], result[vulnerability]["success"], result[vulnerability]["description"])
+
+    db_session.commit()
+
+    
