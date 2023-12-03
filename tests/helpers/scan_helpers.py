3d6976f2096c2111fd311983ef4f5d9f34cb1caa
SPD3 <sean@ayerdoyles.org>
2023-12-02 21:41:34 -0500

    got rid of scan id constants

--- tests/helpers/scan_helpers.py
+++ tests/helpers/scan_helpers.py
@@ -4,4 +4,4 @@ from tests.constants import MICKEY_WEBSITE_NAME, MICKEY_PASSWORD, MICKEY_FIRST_N
 
 def add_mock_scan(db_session, email, url, deep, date_added, website_name=MICKEY_WEBSITE_NAME, password=MICKEY_PASSWORD, first_name=MICKEY_FIRST_NAME, last_name=MICKEY_LAST_NAME, image=MICKEY_IMAGE):
     add_mock_website(db_session, url, email, website_name, password, first_name, last_name, image)
-    add_scan(db_session, email, url, deep, date_added)
+    return add_scan(db_session, email, url, deep, date_added)
